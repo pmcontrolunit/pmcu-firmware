bbb0072bbadb7b3ba5f53bf6776336c9d4ea1d94
Lorenzo Rutayisire <loryruta23@gmail.com>
2019-04-05 15:35:31 +0200

    Add malloc to circular_buffer

--- src/circular_buffer.c
+++ src/circular_buffer.c
@@ -1,9 +1,12 @@
 #include "circular_buffer.h"
+#include <stdlib.h>
 
-void circular_buffer_init(circular_buffer *buffer) {
+void circular_buffer_init(circular_buffer *buffer, unsigned int size) {
     buffer->count = 0;
     buffer->write_position = 0;
     buffer->read_position = 0;
+    buffer->size = size;
+    buffer->data = (unsigned char *) malloc(size * sizeof(unsigned char));
 }
 
 int circular_buffer_is_empty(circular_buffer *buffer) {
@@ -11,25 +14,31 @@ int circular_buffer_is_empty(circular_buffer *buffer) {
 }
 
 int circular_buffer_is_full(circular_buffer *buffer) {
-    return buffer->count == CIRCULAR_BUFFER_DATA_SIZE;
+    return buffer->count == buffer->size;
 }
 
 int circular_buffer_write(circular_buffer *buffer, unsigned char element) {
-    if (buffer->count < CIRCULAR_BUFFER_DATA_SIZE) {
+    if (buffer->count < buffer->size) {
         buffer->count++;
         buffer->data[buffer->write_position] = element;
-        buffer->write_position = ++buffer->write_position < CIRCULAR_BUFFER_DATA_SIZE ? buffer->write_position : 0;
+        buffer->write_position = ++buffer->write_position < buffer->size ? buffer->write_position : 0;
         return 1;
     }
     return 0;
 }
 
 int circular_buffer_read(circular_buffer *buffer, unsigned char *element) {
-    if (buffer->count > 0) {
+    if (buffer->count > buffer->size) {
         buffer->count--;
         *element = buffer->data[buffer->read_position];
-        buffer->read_position = ++buffer->read_position < CIRCULAR_BUFFER_DATA_SIZE ? buffer->read_position : 0;
+        buffer->read_position = ++buffer->read_position < buffer->size ? buffer->read_position : 0;
         return 1;
     }
     return 0;
+}
+
+void circular_buffer_clear(circular_buffer *buffer) {
+    buffer->count = 0;
+    buffer->write_position = 0;
+    buffer->read_position = 0;
 }
