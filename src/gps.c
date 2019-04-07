70fecb8908524bd1362ad1574ee087231f4df5a9
Lorenzo Rutayisire <loryruta23@gmail.com>
2019-04-07 16:37:02 +0200

    Fix UART write (no DMA used)

--- src/gps.c
+++ src/gps.c
@@ -3,8 +3,10 @@
 #include "uart.h"
 #include <msp430.h>
 
+/*
 int gps_read_sentence(unsigned char *sentence_type, unsigned char *buffer, unsigned int buffer_offset, unsigned int buffer_length) {
     uart_read_until(uart_a0, sentence_type, 6, 0, 0, 0);
     uart_read_until(uart_a0, "$", 0, buffer, buffer_offset, buffer_length);
     return 0;
 }
+*/
--- src/uart_test.c
+++ src/uart_test.c
@@ -2,34 +2,22 @@
 #include "uart.h"
 
 int main() {
-    unsigned char byte;
-
     unsigned char message[256];
-    unsigned int i = 0;
 
     WDTCTL = WDTPW | WDTHOLD;
 
     __bis_SR_register(GIE);
 
-    uart_setup(uart_a1, uart_baud_rate_9600);
-    while (1) {
-        if (!uart_write_buffer(uart_a1, "Type something: ", 16) < 16) {
-            __no_operation();
-        }
+    uart_setup(UART_A1, UART_BAUD_RATE_9600_SMCLK_1MHZ);
 
-        while (1) {
-            if (uart_read(uart_a1, &byte)) {
-                message[i++] = byte;
-                if (byte == '\r' || byte == '\n') {
-                       break;
-                }
-            } else {
-                __no_operation();
-            }
-        }
-        uart_write_buffer(uart_a1, "You just wrote:\n", 16);
-        uart_write_buffer(uart_a1, message, i);
-        i = 0;
+    while (1) {
+        uart_write_string(UART_A1, "Type something:\r\n");
+
+        unsigned int size = uart_read_line(UART_A1, message, 256);
+        message[size] = '\0';
+
+        uart_write_string(UART_A1, "You wrote: ");
+        uart_write_string(UART_A1, message);
     }
 }
 
