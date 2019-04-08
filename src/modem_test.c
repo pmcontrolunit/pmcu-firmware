23678c6a41b7e524db63d9cae93374cb9d5a2be0
Lorenzo Rutayisire <loryruta23@gmail.com>
2019-04-08 22:29:37 +0200

    Working SIM800L modem implementation

--- src/modem_test.c
+++ src/modem_test.c
@@ -2,30 +2,13 @@
 #include "uart.h"
 
 /*
-void init_12mhz_mclk() {
-    UCSCTL3 |= SELREF_2;
-
-    __bis_SR_register(SCG0);
-    UCSCTL0 = 0x0000;
-    UCSCTL1 = DCORSEL_5;
-    UCSCTL2 = FLLD_1 + 374;
-    __bic_SR_register(SCG0);
-
-    __delay_cycles(375000);
-}
-
-unsigned char received;
-
 int main () {
     WDTCTL = WDTPW | WDTHOLD;
 
-    // init 12mhz clock, needed for 115200 baud-rate communication
-    init_12mhz_mclk();
-
     __bis_SR_register(GIE);
 
-    uart_setup(UART_A0, UART_BAUD_RATE_115200_SMCLK_12MHZ);
-    uart_setup(UART_A1, UART_BAUD_RATE_115200_SMCLK_12MHZ);
+    uart_setup(UART_A0, UART_BAUD_RATE_9600_SMCLK_1MHZ);
+    uart_setup(UART_A1, UART_BAUD_RATE_9600_SMCLK_1MHZ);
 
     __bis_SR_register(LPM0_bits);
 
--- src/sim800l_test.c
+++ src/sim800l_test.c
@@ -3,19 +3,27 @@
 
 #include <string.h>
 
+/*
 int main() {
-    volatile int error;
+    char result[256];
+    const char *data = "HELLO WORLD!";
+
     WDTCTL = WDTPW | WDTHOLD;
 
     __bis_SR_register(GIE);
 
-    error = modem_init();
-
-    if (!modem_connect("51.77.151.174", "8080")) {
-        const char *data = "HELLO WORLD!";
-        modem_sendall(NULL, data, strlen(data), 0);
+    if (modem_init()) {
+        __no_operation();
     }
+
+    if (modem_connect("51.77.151.174", "8080") {
+        __no_operation();
+    }
+    modem_send(NULL, data, strlen(data), 0);
+
+    modem_disconnect();
 
     __bis_SR_register(LPM0_bits);
     __no_operation();
 }
+*/
